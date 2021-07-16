49ebda713aa3a25ea9a4a3e5f8ade5c3343b64d0
phil <phil@flexsystem.com>
2021-07-16 15:43:56 +0800

    + add comment id to comments list

--- app/Template/comment/show.php
+++ app/Template/comment/show.php
@@ -6,7 +6,7 @@
         <?php if (! empty($comment['username'])): ?>
             <strong class="comment-username"><?= $this->text->e($comment['name'] ?: $comment['username']) ?></strong>
         <?php endif ?>
-
+        <i class="fa fa-comments"><?= $comment['id'] ?></i>
         <small class="comment-date"><?= t('Created at:') ?> <?= $this->dt->datetime($comment['date_creation']) ?></small>
         <small class="comment-date"><?= t('Updated at:') ?> <?= $this->dt->datetime($comment['date_modification']) ?></small>
     </div>
