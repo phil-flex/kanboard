80f1281a5bd83eebccef2d77f7890095de609d63
phil <phil@flexsystem.com>
2022-12-01 15:53:11 +0800

    added 🆔 for each comments

--- app/Template/comment/show.php
+++ app/Template/comment/show.php
@@ -6,7 +6,7 @@
         <?php if (! empty($comment['username'])): ?>
             <strong class="comment-username"><?= $this->text->e($comment['name'] ?: $comment['username']) ?></strong>
         <?php endif ?>
-        KB#<?= $task['id'] ?>&nbsp;💬<?= $comment['id'] ?></i>
+        🆔KB#<?= $task['id'] ?>&nbsp;💬<?= $comment['id'] ?></i>
         <small class="comment-date"><?= t('Created at:') ?> <?= $this->dt->datetime($comment['date_creation']) ?></small>
         <small class="comment-date"><?= t('Updated at:') ?> <?= $this->dt->datetime($comment['date_modification']) ?></small>
     </div>
